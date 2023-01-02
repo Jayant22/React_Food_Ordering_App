ed43a40d7a8e92cc8bd87707f4f1af9e8289c162
Jayant Kodam <38130397+Jayant22@users.noreply.github.com>
2023-01-02 13:05:03 +0530

    Added Comments

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
   };
 
   return (
+    // Using the Context to Store the Data and providing the Data to all Components
     <CartProvider>
       {cartIsActive && <Cart onClose={deactivateCart} />}
       <Header onActiveCart={activateCart} />
